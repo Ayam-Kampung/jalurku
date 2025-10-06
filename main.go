9053c8aeaf730f5a88d3442b3428f3763740eaf7
Saladin <saladinoctianobonanza@gmail.com>
2025-10-06 21:36:35 +0700

    feat(layout & copy): rencanaku

--- main.go
+++ main.go
@@ -1,30 +1,42 @@
 package main
 
 import (
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/template/html/v2"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/html/v2"
 )
 
 func main() {
-    // Berkas Template seperti .html
+	// Berkas Template seperti .html
 	// Template digunakan untuk patokan isi konten yang akan ditampilkan
 	// Templatenya nanti akan bisa diubah isinya
-    engine := html.New("./views", ".html")
+	engine := html.New("./views", ".html")
 
-    app := fiber.New(fiber.Config{
-        Views: engine,
-    })
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
 
-    // Menyediakan file static seperti .css, .js, dll
-    app.Static("/", "./static")
+	// Menyediakan file static seperti .css, .js, dll
+	app.Static("/", "./static")
 
-    // Rute / (root)
+	// Rute / (root)
 	// Kita mengubah {{.Title}} menjadi "Website Sekolah"
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.Render("index", fiber.Map{
-            "Title": "Ayam Kampung—Beranda",
-        }, "layouts/main")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Render("index", fiber.Map{
+			"Title": "Ayam Kampung—Beranda",
+		}, "layouts/main")
+	})
 
-    app.Listen(":3000")
+	app.Get("/rencanaku", func(c *fiber.Ctx) error {
+		return c.Render("rencanaku", fiber.Map{
+			"Title": "Ayam Kampung—RencanaKu",
+		}, "layouts/main")
+	})
+
+	app.Get("/rencanaku/rpl", func(c *fiber.Ctx) error {
+		return c.Render("rpl", fiber.Map{
+			"Title": "Ayam Kampung—RencanaKu",
+		}, "layouts/main")
+	})
+
+	app.Listen(":3000")
 }
