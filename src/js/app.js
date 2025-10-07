5130b82c5c0b2f23d7de971bbb4ff77eea07b5e2
Saladin <saladinoctianobonanza@gmail.com>
2025-10-07 21:16:03 +0700

    feat: animations gsap

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,29 @@
-console.log("Hello from esbuild!");
+import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+
+gsap.registerPlugin(SplitText);
+
+gsap.set("#heading", { opacity: 1 });
+
+let split = SplitText.create("#heading", { type: "chars" });
+gsap.from(split.chars, {
+    y: 20,
+    autoAlpha: 0,
+    stagger: 0.05
+});
+
+gsap.from("#slideText", {
+    x: -300,
+    opacity: 0,
+    duration: 2,
+    ease: "power1.inOut",
+    stagger: 0.5
+})
+
+gsap.from("#slideImages", {
+    x: 300,
+    opacity: 0,
+    duration: 2,
+    ease: "power1.inOut",
+    stagger: 0.5
+})
