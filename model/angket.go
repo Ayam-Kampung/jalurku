421e9820251bec090e481303347062c11811c600
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2025-10-12 11:58:25 +0700

    refactor: merapihkan spasi tab

--- model/angket.go
+++ model/angket.go
@@ -10,48 +10,50 @@ import (
 
 // Jurusan seperti TJA, TKJ, RPL, PG
 type Jurusan struct {
-	ID         	int            		`gorm:"primaryKey;autoIncrement" json:"id"`
-	Name	   	string         		`gorm:"type:varchar(50);unique;not null" json:"name"` 
-	CreatedAt  	time.Time
-	UpdatedAt  	time.Time
+	ID				int				`gorm:"primaryKey;autoIncrement" json:"id"`
+	Name			string			`gorm:"type:varchar(50);unique;not null" json:"name"` 
+	CreatedAt		time.Time
+	UpdatedAt		time.Time
 
-	// Relasi
-	Pertanyaan  []Pertanyaan  		`gorm:"foreignKey:JurusanID"`
-	HasilAngket []HasilAngket 		`gorm:"foreignKey:JurusanID"`
+	// Relasi Jurusan dengan Pertanyaan dan Angket
+	Pertanyaan		[]Pertanyaan	`gorm:"foreignKey:JurusanID"`
+	HasilAngket		[]HasilAngket	`gorm:"foreignKey:JurusanID"`
 }
 
 // Bentuk pertanyaan yang berhubungan dengan jurusan
 type Pertanyaan struct {
-	ID        	uuid.UUID           `gorm:"type:char(36);primaryKey" json:"id"`
-	Text      	string         		`gorm:"type:text;not null" json:"text"`
-	Image		string				`json:"image"`
-	JurusanID 	int            		`gorm:"not null" json:"jurusan_id"`
-	CreatedAt 	time.Time
-	UpdatedAt 	time.Time
+	ID				uuid.UUID		`gorm:"type:char(36);primaryKey" json:"id"`
+	Text			string			`gorm:"type:text;not null" json:"text"`
+	Image			string			`json:"image"`
+	JurusanID		int				`gorm:"not null" json:"jurusan_id"`
+	CreatedAt		time.Time
+	UpdatedAt		time.Time
 
-	Jurusan 	Jurusan 			`gorm:"foreignKey:JurusanID"`
+	// Relasi Pertanyaan dengan Jurusan
+	Jurusan			Jurusan			`gorm:"foreignKey:JurusanID"`
 }
 
 // Hasil angket yang berhubungan dengan pengguna
 type HasilAngket struct {
-	ID        	uuid.UUID      		`gorm:"type:char(36);primaryKey" json:"id"`
-	UserID    	uuid.UUID      		`gorm:"type:char(36);not null" json:"user_id"`
-	JurusanID 	int      		    `gorm:"not null" json:"jurusan_id"` // Ubah ke int
-	CreatedAt 	time.Time
-	UpdatedAt 	time.Time
-	DeletedAt 	gorm.DeletedAt 		`gorm:"index"`
+	ID				uuid.UUID		`gorm:"type:char(36);primaryKey" json:"id"`
+	UserID			uuid.UUID		`gorm:"type:char(36);not null" json:"user_id"`
+	JurusanID		int				`gorm:"not null" json:"jurusan_id"` // Ubah ke int
+	CreatedAt		time.Time
+	UpdatedAt		time.Time
+	DeletedAt		gorm.DeletedAt	`gorm:"index"`
 
-	User    	User    			`gorm:"foreignKey:UserID"`
-	Jurusan 	Jurusan 			`gorm:"foreignKey:JurusanID"`
+	// Relasi Hasil Angket dengan Pengguna dan Jurusan
+	User			User			`gorm:"foreignKey:UserID"`
+	Jurusan			Jurusan			`gorm:"foreignKey:JurusanID"`
 }
 
 type SubmitRequest struct {
-	SessionID       string `json:"session_id"`
-	QuestionID   string `json:"question_id"`
-	SelectedOption int   `json:"selected_option"`
+	SessionID		string			`json:"session_id"`
+	QuestionID		string			`json:"question_id"`
+	SelectedOption	int				`json:"selected_option"`
 }
 
-// Tambahkan data Jurusan -> (1:PG, 2:RPL, 3:TKJ, 4:TJA)
+// Tambahkan data Jurusan terlebih dahulu -> (1:PG, 2:RPL, 3:TKJ, 4:TJA)
 func SeedJurusan(db *gorm.DB) {
 	// Hapus semua data jurusan
 	db.Exec("DELETE FROM jurusan")
