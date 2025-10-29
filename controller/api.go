228d34ae52989cd3c72f5880b4c32bcc62d633e5
Ega-telkom <541241041@student.smktelkom-pwt.sch.id>
2025-10-29 10:30:00 +0700

    test CI/CD part #4

--- controller/api.go
+++ controller/api.go
@@ -10,5 +10,5 @@ import (
 
 // Contoh API
 func Hello(c *fiber.Ctx) error {
-	return c.JSON(fiber.Map{"status": "success", "message": "Halo, dari tim Ayam Kampung! ❤️", "data": nil})
+	return c.JSON(fiber.Map{"status": "success", "message": "Halo, dari tim Ayam Kampung! ❤️", "data": "Untuk JHIC"})
 }
